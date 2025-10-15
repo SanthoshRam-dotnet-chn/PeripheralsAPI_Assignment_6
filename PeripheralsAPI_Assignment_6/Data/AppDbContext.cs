9e54f0a9f3f9eae890da5a564513a229166fedec
Santhosh Ram R <santhosh1708ram@gmail.com>
2025-10-15 17:59:11 +0530

    Add project files.

---
+++ PeripheralsAPI_Assignment_6/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeripheralsAPI_Assignment_6.Models;
+
+namespace PeripheralsAPI_Assignment_6.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Peripheral> Peripherals { get; set; }
+    }
+}
